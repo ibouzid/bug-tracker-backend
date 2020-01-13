a6548902de81478860a5f05e9ebbca4b4afd0efc
Izzy <ibouzid3@gmail.com>
2020-01-13 05:03:10 -0500

    added auth to all api calls

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post('/login', (req,res)=>{
 
 
 //GET ISSUES
-app.get('/issues', (req,res)=>{
+app.get('/issues', verifyToken, (req,res)=>{
     jwt.verify(req.token, process.env.SECRET_OR_KEY, (err, authData)=>{
         if(err){
             res.sendStatus(403);
@@ -96,17 +96,24 @@ app.get('/issues', (req,res)=>{
 
 //GET ISSUE BY ID
 
-app.get('/projects/:projectId/issues/:issueId', (req, res)=>{
-    const GET_ISSUE = `SELECT * FROM issues WHERE issueId="${req.params.issueId}" AND projectId="${req.params.projectId}" `
-    connection.query(GET_ISSUE, (err, results)=>{
+app.get('/projects/:projectId/issues/:issueId',verifyToken, (req, res)=>{
+    jwt.verify(req.token, process.env.SECRET_OR_KEY, (err, authData)=>{
         if(err){
-            return res.send(err)
-        }else{
-            return res.json({
-                data:results
+            return res.sendStatus(403)
+        } else{
+            const GET_ISSUE = `SELECT * FROM issues WHERE issueId="${req.params.issueId}" AND projectId="${req.params.projectId}" `
+            connection.query(GET_ISSUE, (err, results)=>{
+                if(err){
+                    return res.send(err)
+                }else{
+                    return res.json({
+                        data:results
+                    })
+                }
             })
         }
     })
+
 })
 //GET ISSUES BY PROJECT ID
 
@@ -220,6 +227,7 @@ app.get('/projects/:projectId', verifyToken, (req,res)=>{
 //GET ALL USER PROJECTS
 
 app.get('/users/:userId/projects', verifyToken, (req, res)=>{
+
     jwt.verify(req.token, process.env.SECRET_OR_KEY, (err, authData)=>{
         if(err){
             return res.sendStatus(403)
@@ -232,7 +240,7 @@ app.get('/users/:userId/projects', verifyToken, (req, res)=>{
                                        WHERE userId='${req.params.userId}'`
             connection.query(SELECT_PROJECTS_BY_USERID, (err, results)=>{
                 if(err){
-                    return res.send(err);
+                    return res.send("hello");
                 }else{
                     return res.json({
                         data:results
@@ -247,81 +255,129 @@ app.get('/users/:userId/projects', verifyToken, (req, res)=>{
 
 
 //EDITS ISSUE DATA
-app.put("/issues/:issueId", (req, res)=>{
-    const {attachment, title, description, severity, status, ticketType, points, userId, createDate, submittedBy} = req.body;
-    const UPDATE_ISSUE = `UPDATE issues 
-                          SET attachment='${attachment}',  title='${title}', issueDescription='${description}', severity='${severity}', status='${status}',
-                                          ticketType='${ticketType}', points='${points}', userId='${userId}', createDate='${createDate}',
-                                          submittedBy='${submittedBy}'
+app.put("/issues/:issueId", verifyToken, (req, res)=> {
+    jwt.verify(req.token, process.env.SECRET_OR_KEY, (err, authData) => {
+        if (err) {
+            return res.sendStatus(403)
+        } else {
+            const {title, issueDescription, severity, status, ticketType, points, userId, submittedBy, lastUpdated} = req.body;
+            const UPDATE_ISSUE = `UPDATE issues 
+                          SET  lastUpdated='${lastUpdated}',  title='${title}', issueDescription='${issueDescription}', severity='${severity}', status='${status}',
+                                          ticketType='${ticketType}', points='${points}', userId='${userId}', submittedBy='${submittedBy}'
                           WHERE issueId='${req.params.issueId}'`
 
-    connection.query(UPDATE_ISSUE, (err, results)=>{
-        if(err){
-            return res.send(err);
-        }else{
-            return  console.log(`succesfully updated issue`)
+            connection.query(UPDATE_ISSUE, (err, results) => {
+                if (err) {
+                    return res.send(err);
+                } else {
+                    return console.log(`succesfully updated issue`)
+                }
+            })
         }
     })
 })
 
 //POST ISSUE
 
-app.post("/issues",  (req,res) => {
-    const {projectId,projectName, attachment, title, issueDescription, severity, status, ticketType, points, userId, createDate, submittedBy} = req.body;
+app.post("/issues", verifyToken,  (req,res) => {
+    jwt.verify(req.token, process.env.SECRET_OR_KEY, (err, authData)=>{
+        if(err){
+            return res.sendStatus(403)
+        }
+        else {
+            const {projectId, lastUpdated, projectName, attachment, title, issueDescription, severity, status, ticketType, points, userId, createDate, submittedBy} = req.body;
 
-    const INSERT_ISSUE_QUERY = `INSERT INTO issues 
-                                                (projectId, projectName, attachment, title, issueDescription, severity, status, ticketType, points, userId, createDate, submittedBy) 
+            const INSERT_ISSUE_QUERY = `INSERT INTO issues 
+                                                (projectId, lastUpdated, projectName, attachment, title, issueDescription, severity, status, ticketType, points, userId, createDate, submittedBy) 
                                                 VALUES 
-                                                ("${projectId}", "${projectName}", "${attachment}", "${title}", "${issueDescription}", "${severity}", "${status}", "${ticketType}",
+                                                ("${projectId}", "${lastUpdated}", "${projectName}", "${attachment}", "${title}", "${issueDescription}", "${severity}", "${status}", "${ticketType}",
                                                 "${points}", "${userId}", "${createDate}", "${submittedBy}")`;
 
 
-    connection.query(INSERT_ISSUE_QUERY, (err, results)=>{
-        if(err){
-            return res.send(err);
-        }else{
-            return  console.log(`succesfully added issue`)
+            connection.query(INSERT_ISSUE_QUERY, (err, results)=>{
+                if(err){
+                    return res.send(err);
+                }else{
+                    return  console.log(`succesfully added issue`)
+                }
+            })
         }
     })
+
 })
 //POST PROJECT
 
-app.post("/projects",  (req,res) => {
-    const {projectDescription,projectName, projectManager, createDate} = req.body;
+app.post("/projects", verifyToken, (req,res) => {
 
-    const INSERT_PROJECT_QUERY = `INSERT INTO issues 
+    jwt.verify(req.token, process.env.SECRET_OR_KEY, (err, authData) => {
+        if (err) {
+            return res.sendStatus(403)
+        } else {
+            const {projectDescription,projectName, projectManager, createDate} = req.body;
+
+            const INSERT_PROJECT_QUERY = `INSERT INTO projects 
                                                 (projectDescription, projectName, projectManager, createDate) 
                                                 VALUES 
                                                 ("${projectDescription}", "${projectName}", "${projectManager}", "${createDate}")`;
 
 
-    connection.query(INSERT_PROJECT_QUERY, (err, results)=>{
-        if(err){
-            return res.send(err);
-        }else{
-            return  console.log(`succesfully added project`)
+            connection.query(INSERT_PROJECT_QUERY, (err, results)=>{
+                if(err){
+                    return res.send(err);
+                }else{
+                    return  console.log(`succesfully added project`)
+                }
+            })
         }
     })
 })
 
 
-
 //DELETE ISSUE
 
-    app.delete('/issues', (req, res)=>{
-        const {issueId} = req.body;
-            const DELETE_ISSUE_QUERY = `DELETE FROM issues WHERE issueId="${issueId}"`;
-            connection.query(DELETE_ISSUE_QUERY, (err, results)=>{
+    app.delete('/issues', verifyToken, (req, res)=>{
+        jwt.verify(req.token, process.env.SECRET_OR_KEY, (err, authData) => {
+            if (err) {
+                return res.sendStatus(403)
+            } else {
+                const {issueId} = req.body;
+                const DELETE_ISSUE_QUERY = `DELETE FROM issues WHERE issueId="${issueId}"`;
+                connection.query(DELETE_ISSUE_QUERY, (err, results)=>{
+                    if(err){
+                        return res.send(err);
+                    }else{
+                        return  res.send(results)
+                    }
+                })
+            }
+        })
+
+
+
+
+    })
+//DELETE ISSUE
+
+app.delete('/projects', verifyToken, (req, res)=>{
+    jwt.verify(req.token, process.env.SECRET_OR_KEY, (err, authData) => {
+        if (err) {
+            return res.sendStatus(403)
+        } else {
+            const {projectId} = req.body;
+            const DELETE_PROJECT_QUERY = `DELETE FROM projects WHERE projectId="${projectId}"`;
+            connection.query(DELETE_PROJECT_QUERY, (err, results)=>{
                 if(err){
                     return res.send(err);
                 }else{
                     return  res.send(results)
                 }
             })
-
-
+        }
     })
 
+
+
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
